bf76e2824b892330e986e2d05ca3d0e8dd927d3a
Marcus Vinícius da Costa Graciano <48135737+marcuscostagraciano@users.noreply.github.com>
2019-07-09 15:51:31 -0300

    Add files via upload

---
+++ Projeto/cadastro.php
@@ -0,0 +1,12 @@
+<?php
+
+$nome= $_POST["nome"];
+$login= $_POST["login"];
+$email= $_POST["email"];
+$senha= $_POST["senha"];
+$senha_cornfirmada= $_POST["senhaconfirmacao"];
+$data_nasc= $_POST["datanascimento"];
+
+
+
+?>
