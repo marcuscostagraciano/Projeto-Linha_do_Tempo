7604c9eaa04f1a13471448f487f0d4107dfbe761
Marcus Vinícius da Costa Graciano <48135737+marcuscostagraciano@users.noreply.github.com>
2019-08-27 09:20:51 -0300

    Add files via upload

--- Projeto/cadastro.php
+++ Projeto/cadastro.php
@@ -1,12 +1,29 @@
 <?php
 
+$dbhost= "localhost";
+$user= "aluno";
+$password= "aluno";
+$dbname= "projetointegrador2info3";
+
+$link= mysqli_connect($dbhost,$user,$password,$dbname) or die("Erro na conexão");
+
 $nome= $_POST["nome"];
-$login= $_POST["login"];
 $email= $_POST["email"];
 $senha= $_POST["senha"];
 $senha_cornfirmada= $_POST["senhaconfirmacao"];
 $data_nasc= $_POST["datanascimento"];
 
+if ($senha!=$senha_cornfirmada)
+{echo "As senhas são diferentes";
+    mysqli_close($link);
+die();}
 
+$query = "INSERT INTO usuario(nome, email, senha, dt_nasc) VALUES('$nome', '$email','$senha','$data_nasc')";
+
+$result = mysqli_query($link, $query) or die("Error querying database.");
+
+echo "Cadastro realizado com sucesso.";
+
+mysqli_close($link);
 
 ?>
